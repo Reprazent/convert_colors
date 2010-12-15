09e96ef93b18cf6f80d6bae981d814b484f6f37c
Bob Van Landuyt <bob.vanlanduyt@scarlet.be>
2010-12-15 12:17:12 +0100

    Converting now works with the HTML string

--- lib/convert_colors.rb
+++ lib/convert_colors.rb
@@ -1,8 +1,13 @@
 module ConvertColors
 	require 'convert_colors/color.rb'
 	require 'convert_colors/collection.rb'
-	def self.color(name)
-		Collection.colors.find {|c| c.name.downcase == name.downcase.strip}
+
+	def self.color(color_id)
+		if color_id[0,1] == "#"
+			Collection.colors.find {|c| c.to_html == color_id}
+		else
+			Collection.colors.find {|c| c.name.downcase == color_id.downcase.strip}
+		end
 	end
 
 	def self.colors
